a9c7adf3314db8e1e950d96d8536171db599c5bc
timB <Tim.Brown@synergy-logistics.co.uk>
2017-03-06 09:12:09 +0000

    tidied setup scripts

--- windows/setupNewLaptop.ps1
+++ windows/setupNewLaptop.ps1
@@ -1,30 +1,42 @@
-# run as admin?
-# https://chocolatey.org/packages?q=shotty
+#run scripts as admin, as we're installing...
 
+#query packages here: https://chocolatey.org/packages?q=flex
+
+#allow scripts to run
 set-executionpolicy remotesigned
 
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#enable ps remoting
+Enable-PSRemoting -Force
 
-choco install notepadplusplus.install -y
-choco install googlechrome -y
-choco install 7zip.install -y
-#choco install shotty -y
-choco install firefox -y
-choco install nmap -y
-#mosquitto
-#team foundation sidekicks 2013
-#tfs administration tool
-choco install vlc -y
-choco install winmerge -y
-#wix
-#gittfs?
+#install chocolatey
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# choco install jdk8 
+#installs 64bit version which doesn't have plugin manager
+choco install notepadplusplus.install 7zip.install firefox filezilla xmlnotepad -y 
 
-#git related
-choco install git -y
-choco install poshgit -y
-choco install tortoisegit -y
+#scm related
+choco install git poshgit tortoisegit tortoisesvn ankhsvn -y
 
-#putty 
-choco install putty -y
+#Microsoft related
+#invoke-command -ComputerName  FA0033194 -ScriptBlock { choco install sql-server-management-studio -y }
+choco install sql-server-management-studio visualstudio2015professional -y 
+ 
+#manual installs:
+	#manually install vs2005, plus patches
+	#install Flex manually / needs licence
+	#install Subclipse (for Flex) manually. See: http://stackoverflow.com/questions/2108351/subclipse-installation-in-flex-builder-3
+	#needs manual chrome install, as #choco install googlechrome -y doesn't work
+
+	
+#optional installs:
+	#choco install putty shotty nmap vlc winmerge -y	
+	
+	#team foundation sidekicks 2013
+	#tfs administration tool
+
+	#mosquitto	
+	#wix
+
+	# choco install jdk8 
+	# choco install jdk7
+
