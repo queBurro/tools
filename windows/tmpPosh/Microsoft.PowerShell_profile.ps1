7df01e1224fe6cd3317b114c8d585f74e59450ad
timB <Tim.Brown@synergy-logistics.co.uk>
2017-04-18 09:04:34 +0100

    adding script example from drive to github

---
+++ windows/tmpPosh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+
+# my profile file
+# http://msdn.microsoft.com/en-us/library/windows/desktop/bb613488(v=vs.85).aspx
+$Host.PrivateData.ErrorForegroundColor = 'green'
+
+set-alias vi "C:\Program Files (x86)\Notepad++\notepad++.exe"
+
+cd ~
+echo "hello tim"
+
+#get-executionPolicy
