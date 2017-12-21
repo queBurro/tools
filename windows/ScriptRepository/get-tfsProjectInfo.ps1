30d48702451e1545bd1222747b0f271a99a3e508
timB <Tim.Brown@synergy-logistics.co.uk>
2017-12-21 14:52:31 +0000

    add some project info

--- windows/ScriptRepository/get-tfsProjectInfo.ps1
+++ windows/ScriptRepository/get-tfsProjectInfo.ps1
@@ -25,3 +25,27 @@ foreach($collection in $sortedCollections) {
 Write-Host
 Write-Host "Total number of project collections" $sortedCollections.Count
 Write-Host "Total number of projects           " $numberOfProjects
+
+#from: https://www.visualstudio.com/en-us/docs/integrate/api/git/repositories
+#get the following:  http://syntfs02:8080/tfs/DefaultCollection/_apis/git/repositories?api-version=1.0
+#http://syntfs02:8080/tfs/DefaultCollection/_apis/projects?api-version=2.0
+#Invoke-WebRequest -Uri http://syntfs02:8080/tfs/DefaultCollection/_apis/git/repositories?api-version=2.0
+
+#from: https://blogs.technet.microsoft.com/heyscriptingguy/2015/10/08/playing-with-json-and-powershell/ 
+$request = 'http://musicbrainz.org/ws/2/artist/5b11f4ce-a62d-471e-81fc-a69a8278c7da?inc=aliases&fmt=json'
+
+#Invoke-WebRequest $request
+
+Invoke-WebRequest $request |
+  ConvertFrom-Json | 
+  #select -ExpandProperty country | 
+  select name, country, disambiguation
+
+echo "hello"
+$cred = Get-Credential
+
+$request = 'http://syntfs02:8080/tfs/DefaultCollection/_apis/git/repositories?api-version=2.0'
+  Invoke-WebRequest $request -Credential $cred
+
+echo "hello again"
+  Invoke-WebRequest $request -UseDefaultCredentials
